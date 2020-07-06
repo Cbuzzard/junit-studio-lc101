53880a51a64a540e9989e2f74d8e21833f252351
Corwin Buzzard <corwin1998@gmail.com>
2020-07-06 13:24:04 -0400

    Finished class 5 studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,8 +9,63 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketsWithStringToLeftReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("String[]"));
+    }
+
+    @Test
+    public void bracketsWithStringToRightReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]String"));
+    }
+
+    @Test
+    public void bracketsSeparatedByStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[String]"));
+    }
+
+    @Test
+    public void multipleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("String"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void twoLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void twoRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
     }
 
 
